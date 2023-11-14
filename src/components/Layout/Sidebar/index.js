db887887a11c914e9f599d832aee2ec9896fea6d
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2023-11-14 11:16:21 +0100

    sidebar styling

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoSubtitle from '../../../assets/images/logo_sub.png'
 import LogoS from '../../../assets/images/logo-s.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Corrected the variable name
 import { faHome,faUser,faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faSkype, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar =()=>{
     return <>
@@ -16,13 +17,50 @@ const Sidebar =()=>{
             <NavLink exact='true' activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact='true' activeclassname="active" className="active-link" to="/about">
+            <NavLink exact='true' activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
             </NavLink>
             <NavLink exact='true' activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
             </NavLink>
         </nav>
+        <ul>
+            <li>
+            <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            target="_blank" //external link :which means that when the link is clicked, it will open the target URL in a new browser tab or window
+            rel="noreferrer" //The rel attribute is used to specify the relationship between the current document and the linked document. "noreferrer" is often used as a security measure to indicate that no referrer information (i.e., the originating page's URL) should be sent to the linked document. This helps protect the privacy of the user.
+          >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" className="anchor-icon"/>
+                </a>
+            </li>
+            <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        </ul>
     </div>
      </>
 }
